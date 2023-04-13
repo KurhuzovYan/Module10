ca0eb7f4ea819b49c5fa69c84b13eef67924e806
Yan Kurhuzov <yan.dasty@gmail.com>
2023-04-13 23:46:29 +0300

    Commit 5: Add class Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        InputStreamExample ise = new InputStreamExample();
+        System.out.println(ise.searchCorrectNumber());
+
+        CountOfWordsInFile cowif = new CountOfWordsInFile();
+        cowif.counter();
+
+        JsonExample je = new JsonExample();
+        je.createArrayFromFile();
+    }
+
+
+}
