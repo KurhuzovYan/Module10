161a3291c1acf649b21e9388f5c4ea09288da06b
Yan Kurhuzov <yan.dasty@gmail.com>
2023-04-14 15:54:55 +0300

    Commit 8: Uncommitted main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-//        InputStreamExample ise = new InputStreamExample();
-//        System.out.println(ise.searchCorrectNumber());
-//
+        InputStreamExample ise = new InputStreamExample();
+        System.out.println(ise.searchCorrectNumber());
+
         CountOfWordsInFile cowif = new CountOfWordsInFile();
         cowif.counter();
-//
-//        JsonExample je = new JsonExample();
-//        je.createArrayFromFile();
+
+        JsonExample je = new JsonExample();
+        je.createArrayFromFile();
     }
-
-
 }
